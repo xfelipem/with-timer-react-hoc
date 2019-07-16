ae5a63b40a83685f9930cd98b9632df2585f207f
Felipe Medina <medina.xavier.felipe@gmail.com>
2019-07-16 13:48:24 -0300

    Added comments

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
-/**
- * THIRD PARTY DEPENDENCIES
- */
+// THIRD PARTY DEPENDENCIES
 import React from 'react';
-/**
- * VARIABLE DECLARATION
- */
+
+// CONSTANTS DECLARATION
 const DEFAULT_WAIT_INTERVAL = 500;
+
+// COMPONENT DECLARATION
 /**
- * COMPONENT DECLARATION
+ * High order function which makes the child component able to create and destroy a timer.
+ * 
+ * @param {Component} ChildComponent component to be wrapped.
+ * @param {Number} waitInterval timer interval in miliseconds.
  */
-const withTimer = (WrappedComponent, waitInterval = DEFAULT_WAIT_INTERVAL) => {
+const withTimer = (ChildComponent, waitInterval = DEFAULT_WAIT_INTERVAL) => {
+  // PRIVATE PROPERTIES DECLARATION
   let timer = null;
 
-  return props => (
-    <WrappedComponent
-      clearTimer={() => clearTimeout(timer)}
-      setTimer={(callback, ...rest) => {
-        timer = setTimeout(() => callback(...rest), waitInterval);
-      }}
-      {...props}
-    />
-  );
+  // PRIVATE METHODS DECLARATION
+
+  /**
+   * Clears a timer.
+   * 
+   * @returns {undefined} void.
+   */
+  const clearTimer = () => clearTimeout(timer);
+  /**
+   * Method to set a timer. The callback will receive the parameters passed as rest.
+   * 
+   * @param {function} callback Function to be executed after the interval.
+   * @param  {...any} rest Parameters passed to the callback function.
+   * @returns {undefined} void.
+   */
+  const setTimer = (callback, ...rest) => {
+    timer = setTimeout(() => callback(...rest), waitInterval);
+  };
+  /**
+   * Wrapped component, take into account that if the child component has clearTimer and setTimer
+   * properties these will be overridden with the HOC props. 
+   * 
+   * @param {Object} props Component properties.
+   */
+  const ComponentWithTimer = props => <ChildComponent {...props, clearTimer, setTimer} />;
+
+  return ComponentWithTimer;
 };
 
 /**
